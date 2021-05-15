ce4e8806f2883364466ab158e205919d9fcaab4f
Dashylikkkkk <solomatina.d2002@yandex.ru>
2021-05-15 16:44:36 +0300

    .

---
+++ 18.4.py
@@ -0,0 +1,17 @@
+
+def bracket_check(a):
+    i = 0
+    for j in a:
+        if '(' == j:
+            i += 1
+        elif ')' == j:
+            i -= 1
+        if i == -1:
+            break
+    if i == 0:
+        print('yes')
+    else:
+        print('no')
+
+
+bracket_check('(())))')
